54383ab9a15438b3918394adce57885a2ecff576
Quang Nguyen <quangnd.edu@gmail.com>
2018-01-24 16:38:24 +0700

    Test with beforeCreate

--- api/models/User.js
+++ api/models/User.js
@@ -4,6 +4,7 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+var bcrypt = require('bcrypt');
 
 module.exports = {
   // A user may have many pets
@@ -14,6 +15,11 @@ module.exports = {
     lastName: {
       type: 'string'
     },
+    password: {
+      type: 'string',
+      minLength: 6,
+      required: true
+    },
 
     //Add reference to pet
     pets: {
@@ -21,6 +27,18 @@ module.exports = {
       via: 'owners',
       dominant: true
     }
+  },
+
+  // Lifecycle Callbacks
+  beforeCreate: function(values, cb) {
+    bcrypt.hash(values.password, 10, function(err, hash) {
+      if (err) cb(err);
+
+      values.password = hash;
+       //calling cb() with an argument returns an error. Useful for canceling the entire operation if some criteria fails.
+      cb();
+
+    });
   }
 
   //See dominance and many to many relationship at
